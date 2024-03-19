6a9198c0945f35b151179e698d6eb959872f2a73
Alan Fischer <alan@spearfischer.net>
2024-03-19 06:44:48 -0600

    Adding ECS

--- game.py
+++ game.py
@@ -1,18 +1,15 @@
+import esper
 import pygame
-import numpy as np
-
-# Initialize Pygame
-pygame.init()
 
 # Game Constants
 TILE_SIZE = 16
-MAP_FILE = 'Dad map.png'
+MAP_FILE = 'dad_map.png'
 TILES = {
-    'green': 'Grass.png',
+    'green': 'grass.png',
     'blue': 'water.png',
-    'gray': 'mountain.png',
+    'gray': 'mountain_1.png',
     'brown': 'town.png',
-    'unknown': 'unknown.png'
+    'unknown': 'tree.png'
 }
 PLAYER_FILE = 'poochi.png'
 
@@ -26,14 +23,61 @@ COLORS = {
     'pink': (255, 0, 255)
 }
 
+# Components
+class Renderable:
+    def __init__(self, image, z):
+        self.image = image
+        self.z = z
+
+class Movable:
+    pass
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# Systems
+class RenderSystem(esper.Processor):
+    def __init__(self, screen):
+        super().__init__()
+        self.screen = screen
+
+    def process(self):
+        entities = [(ent, *comp) for ent, comp in esper.get_components(Position, Renderable)]
+        entities.sort(key=lambda x: x[-1].z if len(x) == 3 else 0)
+        for ent, position, renderable in entities:
+            self.screen.blit(renderable.image, (position.x, position.y))
+
+class MovementSystem(esper.Processor):
+    def __init__(self, world_map, world_size):
+        super().__init__()
+        self.world_map = world_map
+        self.world_size = world_size
+        self.move_map = {
+            pygame.K_LEFT: (-1, 0),
+            pygame.K_RIGHT: (1, 0),
+            pygame.K_UP: (0, -1),
+            pygame.K_DOWN: (0, 1),
+        }
+
+    def process(self):
+        keys = pygame.key.get_pressed()
+        for entity, (movable, position) in esper.get_components(Movable, Position):
+            for key, (dx, dy) in self.move_map.items():
+                if keys[key]:
+                    new_x = max(0, min(position.x + dx * TILE_SIZE, (self.world_size[0] - 1) * TILE_SIZE))
+                    new_y = max(0, min(position.y + dy * TILE_SIZE, (self.world_size[1] - 1) * TILE_SIZE))
+                    position.x, position.y = new_x, new_y
+
+
+# Initialize Pygame
+pygame.init()
+
 # Load World Map
 world_map = pygame.image.load(MAP_FILE)
 world_size = world_map.get_size()
-tiles = {}
-
-# Load Tiles
-for color, file in TILES.items():
-    tiles[color] = pygame.image.load(file)
+tiles = {color: pygame.image.load(file) for color, file in TILES.items()}
 
 # Load Player
 player_img_ = pygame.image.load(PLAYER_FILE)
@@ -43,28 +87,36 @@ player_pos = [0, 0]
 
 # Create Game Window
 screen = pygame.display.set_mode((world_size[0]*TILE_SIZE, world_size[1]*TILE_SIZE))
-pygame.display.set_caption('Tile Based Game')
+pygame.display.set_caption('Adventures of Poochi')
+
+# Setup ECS
+render_system = RenderSystem(screen)
+movement_system = MovementSystem(world_map, world_size)
+esper.add_processor(render_system)
+esper.add_processor(movement_system)
+
+# Create Player Entity
+player_entity = esper.create_entity()
+esper.add_component(player_entity, Renderable(player_img, 1))
+esper.add_component(player_entity, Movable())
+esper.add_component(player_entity, Position(*player_pos))
 
 def get_tile(color):
     for key, value in COLORS.items():
         if value == color:
-            return tiles.get(key)
-    return tiles.get('unknown')
+            return tiles.get(key, tiles['unknown'])
+        else:
+            return tiles['unknown']
 
-def can_move(pos):
-    x, y = pos
-    if x < 0 or y < 0 or x >= world_size[0] or y >= world_size[1]:
-        return False
-    color = world_map.get_at((x, y))
-    return color != COLORS['blue'] and color != COLORS['gray']
 
-def find_player_start():
-    for x in range(world_size[0]):
-        for y in range(world_size[1]):
-            if world_map.get_at((x, y)) == COLORS['pink']:
-                return [x, y]
-
-    return [0, 0]
+# Create Tile Entities
+for x in range(world_size[0]):
+    for y in range(world_size[1]):
+        tile_entity = esper.create_entity()
+        tile_color = world_map.get_at((x, y))
+        tile = get_tile(tile_color)
+        esper.add_component(tile_entity, Renderable(tile, 0))
+        esper.add_component(tile_entity, Position(x * TILE_SIZE, y * TILE_SIZE))
 
 def game_loop():
     running = True
@@ -72,34 +124,14 @@ def game_loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                x, y = player_pos
-                if event.key == pygame.K_LEFT:
-                    if can_move((x - 1, y)):
-                        player_pos[0] -= 1
-                elif event.key == pygame.K_RIGHT:
-                    if can_move((x + 1, y)):
-                        player_pos[0] += 1
-                elif event.key == pygame.K_UP:
-                    if can_move((x, y - 1)):
-                        player_pos[1] -= 1
-                elif event.key == pygame.K_DOWN:
-                    if can_move((x, y + 1)):
-                        player_pos[1] += 1
 
-        # Render
-        screen.fill((0, 0, 0))
-        for x in range(world_size[0]):
-            for y in range(world_size[1]):
-                tile_color = world_map.get_at((x, y))
-                tile = get_tile(tile_color)
-                if tile:
-                    screen.blit(tile, (x * TILE_SIZE, (y - 32) * TILE_SIZE))
-        screen.blit(player_img, (player_pos[0] * TILE_SIZE, (player_pos[1] - 32) * TILE_SIZE))
+        # Update ECS world
+        esper.process()
+
+        # Flip display
         pygame.display.flip()
 
     pygame.quit()
 
-player_pos = find_player_start()
+# Start the game
 game_loop()
-
